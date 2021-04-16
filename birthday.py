c9713c2a6ae1b34de82518c48f3efa06eb133a7a
rubbb19 <82602017+rubbb19@users.noreply.github.com>
2021-04-16 12:25:09 -0500

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,15 @@
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+#Try it yourself
+print(5+3)
+print(4*2)
+print(24/3)
+print(100-92)
+
+#Favourite Number
+favorite_number = 19
+message = "My favorite number is " + str(favorite_number)
+print(message)
